d6ab9cda6ec3ebdd5d900d2ba4cb0deb870bc616
Jpineapple <Jpineapple@users.noreply.github.com>
2015-06-15 16:35:19 -0400

    Code Cleanup

--- Main.py
+++ Main.py
@@ -9,33 +9,33 @@ class Win(Tkinter.Tk):
         self.initialize()
         
     def initialize(self):
-        self.stats = Tkinter.Frame(self)
-        self.stats.pack(side="left", fill="both", expand=1)
-
-        self.dex = LabelEntry(self.stats, "Dex", 0, 0)
-        self.critc = LabelEntry(self.stats, "Crit Chance", 1, 0)
-        self.critdmg = LabelEntry(self.stats, "Crit Damage", 1, 2)
-        
-
+        self.stats_frame = Tkinter.Frame(self)
+        self.stats_frame.pack(side="left", fill="both", expand=1)
             
         DataControl = DataController()
         labent_data = DataControl.gui_data()
+        self.entry_store = []
         for i, k in enumerate(labent_data):
             label, col, row = k
-            LabelEntry(self.stats, label, col, row)
+            temp = LabelEntry(self.stats_frame, label, col, row)
+            self.entry_store.append(temp)
 
         self.res = Tkinter.Frame(self)
         self.res.pack(side="left", fill="y", expand=1)
         
-        self.button = Tkinter.Button(self.res, text="calculate", command=self.callback)
+        self.button = Tkinter.Button(self.res, text="calculate",
+                                     command=self.callback)
         self.button.pack(side="bottom")
         
     def callback(self):
-        print self.dex.entryvar.get()
-        print self.critc.entryvar.get()
-        print self.critdmg.entryvar.get()
-        
-        
+        self.entrylist = {}
+        for i, k in enumerate(self.entry_store):
+            try:
+                int(k.entryvar.get())
+                self.entrylist[k.label.text] = k.entryvar.get()
+            except:
+                print "ERROR: Value for " + k.label.text + " not set."
+        print self.entrylist
         
 if __name__ == "__main__":
     app = Win(None)
--- compound_widgets.py
+++ compound_widgets.py
@@ -1,30 +1,38 @@
 #Compound Widgets
-# cd C:\git\D3stat
 import Tkinter
 
 
 class LabelEntry(Tkinter.Tk):    
     def __init__(self, parent, label, column=0, row=0, entrydef="Set"):
         self.parent = parent
+        self.entrydef = entrydef
         self.container(parent, column, row)
-        self.gen_label(parent, label, column, row)
-        self.gen_entry(parent, column, row, entrydef)
+        self.gen_label(parent, label)
+        self.gen_entry(parent, entrydef)
         
     def container(self, parent, column, row):
         self.container = Tkinter.Frame(parent)
         self.container.grid(column=column, row=row)
         
-    def gen_label(self, parent, label, column, row):
+    def gen_label(self, parent, label):
         self.label = Tkinter.Label(self.container, text=label)
+        self.label.text = label
         self.label.pack(side="top", fill="both", expand=1)
         
-    def gen_entry(self, parent, column, row, entrydef):
+    def gen_entry(self, parent, entrydef):
         self.entryvar = Tkinter.StringVar(value=entrydef)
         self.entry = Tkinter.Entry(self.container, bg="yellow", textvariable=self.entryvar)
         self.entry.pack(side="top", fill="both", expand=1)
         
-        self.entry.bind("<Button-1>", lambda event, v=self.entryvar: self.onclick(event, v))
+        self.entry.bind("<Button-1>", self.onclick)
+        self.entry.bind("<FocusOut>", self.onleave)
 
-    def onclick(self, event, var):
-        self.entryvar.set("")
+    def onclick(self, event):
+        try:
+            int(event.widget.get())
+        except:
+            self.entryvar.set("")
         
+    def onleave(self, event):
+        if self.entry.get() is "":
+            self.entryvar.set(self.entrydef)
